f6ddd25da033e794d00591813ad142b460277cf7
kienonline19 <kienonline19.haui@gmail.com>
2023-12-12 22:55:04 +0700

    update 17

--- yinyang.py
+++ yinyang.py
@@ -1,3 +1,40 @@
+"""
+Đây là đoạn code Python sử dụng thư viện turtle để vẽ biểu tượng Yin Yang. Chúng ta hãy phân tích từng phần:
+
+Đầu tiên import thư viện turtle:
+
+```python
+from turtle import *
+```
+
+Tiếp theo định nghĩa hàm `yin()` để vẽ nửa biểu tượng Yin hoặc Yang:
+
+```python
+def yin(radius, color1, color2):
+```
+
+Các tham số truyền vào:
+
+- `radius`: bán kính vòng tròn lớn
+- `color1`: màu nền
+- `color2`: màu vòng tròn nhỏ bên trong
+
+Bên trong hàm `yin()`:
+
+- Dùng các hàm `width()`, `color()`, `begin_fill()`, `circle()`,... để vẽ nửa hình tròn với màu nền `color1`
+- Vẽ vòng tròn nhỏ màu `color2` bên trong
+- Dùng các hàm `up()`, `forward()`, `backward()` để di chuyển con trỏ turtle
+
+Tiếp theo định nghĩa hàm `main()` để vẽ toàn bộ biểu tượng Yin Yang:
+
+- Gọi `yin()` 2 lần với các màu nền đối lập đen/trắng
+- Dùng `ht()` ẩn con trỏ turtle
+- Trả về chuỗi `Done!`
+
+Cuối cùng gọi `main()` nếu file được chạy trực tiếp.
+
+Như vậy code vẽ biểu tượng Yin Yang bằng Python turtle.
+"""
 from turtle import *
 
 
